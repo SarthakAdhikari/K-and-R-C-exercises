2fac374d213efe5ce8b39990a0684a9904c10bb2
Sarthak Adhikari <adhikari.sarthak@gmail.com>
2018-04-04 14:51:22 +0545

    Added Exercise 1.10

---
+++ chatper_1/exercise_10/replace_input_to_output.c
@@ -0,0 +1,35 @@
+/* Replace tab by \t, backspace by \b, and backslash by \\ on input*/
+
+#include<stdio.h>
+#define TAB 9
+#define BACKSPACE 8
+#define BACKSLASH 92
+
+int main(){
+   int get_char_value, condition_counter;
+   
+
+   while((get_char_value = getchar()) != EOF){
+       condition_counter = 0;
+       if(get_char_value == TAB){
+           putchar('\\');
+           putchar('t');
+           condition_counter = 1;
+       }
+       if(get_char_value == BACKSPACE){
+           putchar('\\');
+           putchar('b');
+           condition_counter = 1;
+       } 
+       if(get_char_value == BACKSLASH){
+           putchar('\\');
+           putchar('\\');
+           condition_counter = 1;
+       }
+       if(condition_counter != 1){
+           putchar(get_char_value);
+       }
+   }
+
+  return 0;
+}
